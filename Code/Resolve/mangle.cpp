7e42cd913cd6613263e452ccdcaeba38db16559b
Rimmer <rimmer.motzheim@gmail.com>
2015-01-26 00:45:22 +0100

    Added simpel name mangling.

---
+++ Code/Resolve/mangle.cpp
@@ -0,0 +1,67 @@
+
+#include "mangle.h"
+
+namespace athena {
+namespace resolve {
+
+Core::String Mangler::mangle(Function* function) {
+	string.Clear();
+	string += "_Z";
+
+	auto name = context.Find(function->name);
+	mangleQualifier(&name);
+
+	// TODO: Mangle argument types.
+	return string;
+}
+
+void Mangler::mangleQualifier(ast::Qualified* qualified) {
+	string += "N";
+	auto name = qualified->name;
+	auto q = qualified->qualifier;
+	while(q) {
+		char buffer[32];
+		Core::NumberToString(q->name.length, buffer, 32);
+		string += &buffer[0];
+		string.Append(q->name.ptr, q->name.length);
+		q = q->qualifier;
+	}
+
+	char buffer[32];
+	Core::NumberToString(name.length, buffer, 32);
+	string += &buffer[0];
+	string.Append(name.ptr, name.length);
+	string += 'E';
+}
+
+void Mangler::mangleType(TypeRef type) {
+	if(type->isUnit())
+		string += 'v';
+	else if(type->isPrimitive())
+		mangleType(((PrimType*)type)->type);
+	else if(type->isPointer())
+		mangleType((PtrType*)type);
+	else if(type->kind == Type::Agg)
+		mangleType((AggType*)type);
+}
+
+void Mangler::mangleType(PrimitiveType type) {
+	static const char types[] = {
+			'x', 'i', 's', 'c',
+			'y', 'j', 't', 'h',
+			'd', 'f', 't', 'b'
+	};
+	string += types[(uint)type];
+}
+
+void Mangler::mangleType(const PtrType* type) {
+	string += 'P';
+	mangleType(type->type);
+}
+
+void Mangler::mangleType(const AggType* type) {
+	auto name = context.Find(type->name);
+	mangleQualifier(&name);
+}
+
+}} // namespace athena::resolve
---
+++ Code/Resolve/mangle.h
@@ -0,0 +1,32 @@
+#ifndef Athena_Resolve_mangle_h
+#define Athena_Resolve_mangle_h
+
+#include "../Parse/parser.h"
+#include "resolve_ast.h"
+
+namespace athena {
+namespace resolve {
+
+struct Mangler {
+	Mangler(ast::CompileContext& context) : context(context) {}
+
+	/// Mangles the name of the provided function.
+	Core::String mangle(Function* function);
+
+	/// Mangles a qualified name.
+	void mangleQualifier(ast::Qualified* qualified);
+
+	/// Mangles a type name.
+	void mangleType(TypeRef type);
+	void mangleType(PrimitiveType type);
+	void mangleType(const PtrType* type);
+	void mangleType(const AggType* type);
+
+private:
+	ast::CompileContext& context;
+	Core::String string;
+};
+
+}} // namespace athena::resolve
+
+#endif // Athena_Resolve_mangle_h
